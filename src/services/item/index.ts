e070c1b79dbc435d79193c44cd2703343454ae80
Yannick Mijsters <y.mijsters@gmail.com>
2024-09-13 10:56:40 +0200

    fix: rebase branch

--- src/services/item/index.ts
+++ src/services/item/index.ts
@@ -19,11 +19,9 @@ import graaspApps from './plugins/app';
 import graaspEmbeddedLinkItem from './plugins/embeddedLink';
 import { PREFIX_EMBEDDED_LINK } from './plugins/embeddedLink/service';
 import graaspEnrollPlugin from './plugins/enroll';
-import graaspEtherpadPlugin from './plugins/etherpad/controller';
 import graaspFileItem from './plugins/file';
 import graaspFolderItem from './plugins/folder/controller';
 import itemGeolocationPlugin from './plugins/geolocation/index';
-import graaspH5PPlugin from './plugins/html/h5p';
 import graaspZipPlugin from './plugins/importExport';
 import graaspInvitationsPlugin from './plugins/invitation';
 import graaspCategoryPlugin from './plugins/itemCategory';
@@ -88,11 +86,6 @@ const plugin: FastifyPluginAsync = async (fastify) => {
       // core routes - require authentication
       fastify.register(async function (fastify) {
         fastify.register(itemWsHooks);
-
-        // H5P plugin must be registered before ZIP
-        fastify.register(graaspH5PPlugin);
-
-        fastify.register(graaspEtherpadPlugin);
 
         fastify.register(graaspZipPlugin);
 
