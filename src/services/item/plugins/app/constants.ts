067b49cd315aa3ab86d984188af87d3f8b4ebf26
LAPTOP-YANNICK\ymijs <y.mijsters@gmail.com>
2024-11-08 15:49:02 +0100

    feat: extend token lifetime for longer experiments

--- src/services/item/plugins/app/constants.ts
+++ src/services/item/plugins/app/constants.ts
@@ -1,4 +1,4 @@
-export const DEFAULT_JWT_EXPIRATION = 30;
+export const DEFAULT_JWT_EXPIRATION = 300;
 
 export const APPS_TEMPLATE_PATH_PREFIX = 'apps/templates/';
 export const THUMBNAILS_ROUTE = '/thumbnails';
